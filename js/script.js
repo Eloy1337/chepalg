ca2c8dc69c91258917b0bb73e2cf8d6a78dbe5b7
Eloy1337 <malsagov.01@mail.ru>
2020-12-01 03:36:38 +0300

    Разработали гланую страницу и музыку

---
+++ js/script.js
@@ -0,0 +1,48 @@
+"use strict"
+
+// function soundClick() {
+//     var audio = new Audio(); // Создаём новый элемент Audio
+//     audio.src = 'nevergonna.mp3'; // Указываем путь к звуку "клика"
+//     audio.autoplay = true; // Автоматически запускаем
+// }
+
+function randomInteger(min, max) {
+    // получить случайное число от (min-0.5) до (max+0.5)
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+}
+
+
+const button = document.querySelector("button");
+
+// audio.addEventListener("play", () => {
+//     console.log("vrode robbit");
+// });
+
+
+let audiomassiv = ["nevergonna.mp3", "without.mp3", "agiga.mp3", "madina.mp3", "hefuckedup.mp3"]
+// document.querySelector('source').src = `music/${audiomassiv[randomInteger(0, audiomassiv.length - 1)]}`
+
+
+let elem = document.createElement('div');
+elem.append(tmpl.content.cloneNode(true));
+elem.querySelector("audio").classList.remove('audiotemplate');
+elem.querySelector("audio").classList.add('audio');
+elem.querySelector("source").src = `music/${audiomassiv[randomInteger(0, audiomassiv.length - 1)]}`;
+document.querySelector("main").append(elem);
+const audio = document.querySelector(".audio");
+
+
+button.textContent = "Просто дай мне чепалг";
+button.addEventListener("click", () => {
+
+    if (button.textContent == "Просто дай мне чепалг") {
+        audio.play();
+        button.textContent = "Больше не надо";
+    } else {
+        audio.pause();
+        button.textContent = "Просто дай мне чепалг";
+    }
+})
+
+console.log("vse norm")
